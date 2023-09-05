a12008f15437719db8d9bbacaff89ee768fe0716
Anastasia Mexa <anastasia.mexa@yahoo.gr>
2023-09-05 19:56:12 +0300

    First use of state

--- code/00-starting-setup/src/components/Expenses/ExpenseItem.js
+++ code/00-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,29 @@
+import React, {useState} from "react";
+
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  /** 
+   * useState returns an array with 2 elements,
+   * the first is the current state value, the second is a function that allows us to update the state value.
+   * useState is a React hook, it allows us to manage state in a functional component.
+   */
+  const [title, setTitle] = useState(props.title); 
+
+  function clickHandler() {
+    setTitle('Updated!');
+  }
+
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
