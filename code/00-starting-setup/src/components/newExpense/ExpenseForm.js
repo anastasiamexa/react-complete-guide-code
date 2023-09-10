5a1f2f43e3e79a1736e4b3021bfa7683a8afe7d5
Anastasia Mexa <anastasia.mexa@yahoo.gr>
2023-09-10 15:50:27 +0300

    Messing around with two-way binding

--- code/00-starting-setup/src/components/newExpense/ExpenseForm.js
+++ code/00-starting-setup/src/components/newExpense/ExpenseForm.js
@@ -17,20 +17,39 @@ function ExpenseForm() {
     }
   };
 
+  function submitHandler(event) {
+    // Prevents the default behavior of the form
+    // (sending a request and reloading the page)
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate)
+    }
+
+    console.log(expenseData);
+
+    // Clear the form after submitting
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
+  }
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
-          <label>{enteredTitle}</label>
-          <input type='text' onChange={(event) => inputChangeHandler('title', event.target.value)}/>
+          <label>Title</label>
+          <input type='text' value={enteredTitle} onChange={(event) => inputChangeHandler('title', event.target.value)}/>
         </div>
         <div className='new-expense__control'>
-          <label>{enteredAmount}</label>
-          <input type='number' min='0.01' step='0.01' onChange={(event) => inputChangeHandler('amount', event.target.value)}/>
+          <label>Amount</label>
+          <input type='number' min='0.01' step='0.01' value={enteredAmount} onChange={(event) => inputChangeHandler('amount', event.target.value)}/>
         </div>
         <div className='new-expense__control'>
-          <label>{enteredDate}</label>
-          <input type='date' min='2019-01-01' max='2022-12-31' onChange={(event) => inputChangeHandler('date', event.target.value)}/>
+          <label>Date</label>
+          <input type='date' min='2019-01-01' max='2024-12-31' value={enteredDate} onChange={(event) => inputChangeHandler('date', event.target.value)}/>
         </div>
       </div>
       <div className='new-expense__actions'>
