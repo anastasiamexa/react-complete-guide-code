c2080c12020cf02fd46440ca3443316ab7810158
Anastasia Mexa <anastasia.mexa@yahoo.gr>
2023-10-10 23:14:35 +0300

    Dummy derived value example

--- code/00-starting-setup/src/components/Expenses/Expenses.js
+++ code/00-starting-setup/src/components/Expenses/Expenses.js
@@ -6,6 +6,19 @@ import "./Expenses.css";
 
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState('2020');
+  
+  // Derived value (state that depends on other state)
+  let filterInfoText = '2019, 2021 & 2022';
+
+  if (filteredYear === '2019') {
+    filterInfoText = '2020, 2021 & 2022';
+  } else if (filteredYear === '2020') {
+    filterInfoText = '2019, 2021 & 2022';
+  } else if (filteredYear === '2021') {
+    filterInfoText = '2019, 2020 & 2022';
+  } else {
+    filterInfoText = '2019, 2020 & 2021';
+  }
 
   function filterChangeHandler(selectedYear) {
     setFilteredYear(selectedYear);
@@ -15,6 +28,7 @@ function Expenses(props) {
     <div>
       <Card className='expenses'>
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}></ExpensesFilter>
+        <p>Data for years {filterInfoText} is hidden.</p>
         <ExpenseItem
           title={props.expenses[0].title}
           amount={props.expenses[0].amount}
